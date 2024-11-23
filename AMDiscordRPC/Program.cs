f9975ae31104f32ad95be6fc4bc9cefb0e839f65
CrawLeyYou <60201017+CrawLeyYou@users.noreply.github.com>
2024-11-23 21:20:45 +0300

    Push 1.1.0.0

--- AMDiscordRPC/Program.cs
+++ AMDiscordRPC/Program.cs
@@ -7,6 +7,7 @@ using System.Net.Http;
 using System.Web;
 using Newtonsoft.Json.Linq;
 using System.Threading.Tasks;
+using FlaUI.Core.AutomationElements;
 
 namespace AMDiscordRPC
 {
@@ -14,32 +15,66 @@ namespace AMDiscordRPC
     {
         public static DiscordRpcClient client;
         public static HttpClient hclient = new HttpClient();
+        public static FlaUI.Core.Application AppleMusic;
+        public static bool AMAttached;
         static void Main(string[] args)
         {
-           InitializeDiscordRPC();
-           AMSongDataEvent.SongChanged += async (sender, x) =>
-            {
-                Console.WriteLine($"Song: {x.SongName}\nArtist and Album: {x.ArtistandAlbumName}");
-                var image = await FetchiTunes(HttpUtility.UrlEncode(x.ArtistandAlbumName.Replace("—", "-")));
-                client.SetPresence(new RichPresence()
-                {
-                    Type = ActivityType.Listening,
-                    Details = x.SongName,
-                    State = x.ArtistandAlbumName.Split('—')[0],
-                    Assets = new Assets()
-                    {
-                        LargeImageKey = image,
-                        LargeImageText = x.ArtistandAlbumName.Split('—')[1],
-                    }
-                });
-            };
-           AMEvent();
+            InitializeDiscordRPC();
+            AttachToAppleMusic();
+            AMSongDataEvent.SongChanged += async (sender, x) =>
+             {
+                 Console.WriteLine($"Song: {x.SongName}\nArtist and Album: {x.ArtistandAlbumName}");
+                 var image = await FetchiTunes(HttpUtility.UrlEncode(x.ArtistandAlbumName.Replace("—", "-")));
+                 client.SetPresence(new RichPresence()
+                 {
+                     Type = ActivityType.Listening,
+                     Details = x.SongName,
+                     State = x.ArtistandAlbumName.Split('—')[0],
+                     Assets = new Assets()
+                     {
+                         LargeImageKey = image,
+                         LargeImageText = x.ArtistandAlbumName.Split('—')[1],
+                     }
+                 });
+             };
+            AMEvent();
         }
 
         static void InitializeDiscordRPC()
         {
             client = new DiscordRpcClient("1308911584164319282");
             client.Initialize();
+        }
+
+        static void AttachToAppleMusic()
+        {
+            try
+            {
+                AppleMusic = FlaUI.Core.Application.Attach("AppleMusic.exe");
+                AMAttached = true;
+                Console.WriteLine("Attached");
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Apple Music not found", e.Message);
+                AMAttached = false;
+                ResetPresenceIdle();
+            }
+        }
+
+        static void ResetPresenceIdle()
+        {
+            client.SetPresence(new RichPresence()
+            {
+                Type = ActivityType.Playing,
+                Details = "Apple Music",
+                State = "Idle",
+                Assets = new Assets()
+                {
+                    LargeImageKey = "",
+                    LargeImageText = "Apple Music",
+                }
+            });
         }
 
         /* I realized we don't need Last.fm API to be here, bc we are making Apple Music RPC aren't we? so i decided to just use iTunes and go on.
@@ -83,7 +118,8 @@ namespace AMDiscordRPC
             try
             {
                 var iTunesReq = await hclient.GetAsync($"https://itunes.apple.com/search?term={ArtistAndAlbum}&limit=1&entity=song");
-                if (iTunesReq.IsSuccessStatusCode) { 
+                if (iTunesReq.IsSuccessStatusCode)
+                {
                     dynamic imageRes = JObject.Parse(await iTunesReq.Content.ReadAsStringAsync());
                     if (imageRes["resultCount"] != 0)
                     {
@@ -95,7 +131,8 @@ namespace AMDiscordRPC
                         Console.WriteLine("iTunes no image found");
                         return string.Empty;
                     }
-                } else
+                }
+                else
                 {
                     Console.WriteLine("iTunes request failed");
                     return string.Empty;
@@ -107,61 +144,121 @@ namespace AMDiscordRPC
                 return string.Empty;
             }
         }
-        static void AMEvent()
+
+        static async void AMEvent()
         {
-            var AppleMusic = FlaUI.Core.Application.Attach("AppleMusic.exe");
             using (var automation = new UIA3Automation())
             {
-                var window = AppleMusic.GetMainWindow(automation);
-                var parent = window.FindFirstChild(cf => cf.ByClassName("Microsoft.UI.Content.DesktopChildSiteBridge")).FindFirstChild().FindFirstChild().FindFirstChild(cf => cf.ByAutomationId("TransportBar")).FindFirstChild(cf => cf.ByAutomationId("LCD"));
-                var listeningInfo = parent.FindAllChildren().Where(x => (x.AutomationId == "myScrollViewer")).ToArray();
-                string previousSong = string.Empty;
-                string previousArtistAlbum = string.Empty;
+                var playingStatus = false;
+                AutomationElement parent = null;
+                AutomationElement[] listeningInfo = null;
 
-                while (true)
+                while (!playingStatus)
                 {
-                   try
+                    // I hate microsoft because of this
+                    try
                     {
-                        var currentSong = listeningInfo[0].Name;
-                        var currentArtistAlbum = listeningInfo[1].Name;
-                        var dashSplit = listeningInfo[1].Name.Split('-');
-                        bool isSingle = dashSplit[dashSplit.Length - 1].Contains("Single");
-
-                        if (currentSong != previousSong || currentArtistAlbum != previousArtistAlbum)
+                        if (AppleMusic.HasExited)
                         {
-                            AMSongDataEvent.SongChange(new SongData(currentSong, (isSingle) ? string.Join("-", dashSplit.Take(dashSplit.Length - 1).ToArray()) : currentArtistAlbum));
-                            previousArtistAlbum = currentArtistAlbum;
-                            previousSong = currentSong;
+                            AMAttached = false;
+                            break;
                         }
+                    }
+                    catch (Exception e)
+                    {
+                        AMAttached = false;
+                        break;
+                    }
 
+                    try
+                    {
+                        var window = AppleMusic.GetMainWindow(automation);
+                        parent = window.FindFirstChild(cf => cf.ByClassName("Microsoft.UI.Content.DesktopChildSiteBridge")).FindFirstChild().FindFirstChild().FindFirstChild(cf => cf.ByAutomationId("TransportBar")).FindFirstChild(cf => cf.ByAutomationId("LCD"));
+                        listeningInfo = parent.FindAllChildren().Where(x => (x.AutomationId == "myScrollViewer")).ToArray();
+                        playingStatus = true;
                     }
                     catch (Exception e)
                     {
                         Console.WriteLine(e.Message);
                     }
-                    Thread.Sleep(50);
+                }
+
+
+                if (AMAttached)
+                {
+                    string previousSong = string.Empty;
+                    string previousArtistAlbum = string.Empty;
+
+                    while (true)
+                    {
+                        if (AppleMusic.HasExited != true)
+                        {
+                            try
+                            {
+                                var currentSong = listeningInfo[0].Name;
+                                var currentArtistAlbum = listeningInfo[1]?.Name;
+                                var dashSplit = listeningInfo[1].Name.Split('-');
+                                bool isSingle = dashSplit[dashSplit.Length - 1].Contains("Single");
+
+                                if (currentSong != previousSong || currentArtistAlbum != previousArtistAlbum)
+                                {
+                                    AMSongDataEvent.SongChange(new SongData(currentSong, (isSingle) ? string.Join("-", dashSplit.Take(dashSplit.Length - 1).ToArray()) : currentArtistAlbum));
+                                    previousArtistAlbum = currentArtistAlbum;
+                                    previousSong = currentSong;
+                                }
+
+                            }
+                            catch (Exception e)
+                            {
+                                Console.WriteLine(e.Message);
+                            }
+                        }
+                        else
+                        {
+                            Console.WriteLine("Process Closed");
+                            AMAttached = false;
+                            ResetPresenceIdle();
+                            while (!AMAttached)
+                            {
+                                AttachToAppleMusic();
+                                Thread.Sleep(1000);
+                            }
+                            AMEvent();
+                        }
+                        Thread.Sleep(50);
+                    }
+                }
+                else
+                {
+                    while (!AMAttached)
+                    {
+                        AttachToAppleMusic();
+                        Thread.Sleep(1000);
+                    }
+                    AMEvent();
                 }
             }
         }
-    }
-    public class AMSongDataEvent
-    {
-        public static event EventHandler<SongData> SongChanged;
-        public static void SongChange(SongData e)
+
+        public class AMSongDataEvent
         {
-            SongChanged?.Invoke(null, e);
+            public static event EventHandler<SongData> SongChanged;
+            public static void SongChange(SongData e)
+            {
+                SongChanged?.Invoke(null, e);
+            }
         }
-    }
 
-    public class SongData : EventArgs
-    {
-        public string SongName { get; set; }
-        public string ArtistandAlbumName { get; set; }
-
-        public SongData(string songName, string artistandAlbumName)
+        public class SongData : EventArgs
         {
-            SongName = songName;
-            ArtistandAlbumName = artistandAlbumName;
+            public string SongName { get; set; }
+            public string ArtistandAlbumName { get; set; }
+
+            public SongData(string songName, string artistandAlbumName)
+            {
+                SongName = songName;
+                ArtistandAlbumName = artistandAlbumName;
+            }
         }
     }
 }
